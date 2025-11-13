742937fe22c2648169019577a41feee974a1184e
Auvryy <sarneandy6@gmail.com>
2025-11-13 14:09:32 +0800

    added components

---
+++ frontend/src/components/button/button.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default Button = () => {
+  return (
+    <>
+      <button className="bg-amber-300 border-2 rounded-2xl px-5 py-3">Click me</button>
+    </>
+  );
+}
+
