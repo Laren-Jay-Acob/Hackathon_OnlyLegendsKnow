aab4f28c54e549a82c59a47cb5cb0cd3977d0b73
Apue <apue2434@gmail.com>
2025-11-14 11:18:45 +0800

    a

--- fapp.py
+++ fapp.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from enum import Enum
 from sqlalchemy import create_engine, Integer, String, Boolean, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, scoped_session, sessionmaker
+from time import sleep
 
 app = Flask(__name__)
 
@@ -52,12 +54,14 @@ class Shop(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     shop_item: Mapped[str] = mapped_column(String(128), nullable=False)
     item_level: Mapped[int] = mapped_column(Integer, default=0)
+    item_price: Mapped[int] = mapped_column(Integer, default=50)
 
     def get_data(self):
         return {
             "id": self.id,
             "shop_item": self.shop_item,
             "item_level": self.item_level,
+            "item_price": self.item_price,
         }
     
 class Coins(Base):
@@ -77,6 +81,20 @@ def run_app():
     @app.teardown_appcontext
     def shutdown_session(exception=None):
         db.remove()
+
+    for ishop in Shops_item:
+        items = Shop(shop_item=ishop.value)
+        db.add(items)
+    
+    coins = Coins(amount=0)
+    user = Player(level=1)
+
+    db.add_all([coins, user])
+    succ, err = commit_sesison()
+    if not succ:
+        return json_resp(500, False, err)
+
+    CORS(app=app)
 
     return app
 
@@ -162,6 +180,24 @@ def shop_level_item():
     
     return json_resp(200, False, "You have leveled up")
 
+@app.route("/shop/item/price", methods=['PATCH'])
+def shop_item_price():
+    data: dict = request.get_json()
+
+    item_id = data.get("id")
+    item_price = data.get("price")
+
+    ishop = db.get(Shop, int(item_id))
+
+    ishop.item_price += int(item_price)
+
+    succ, err = commit_sesison()
+    if not succ:
+        return json_resp(500, False, err)
+    
+    return json_resp(200, False, "You have leveled up")
+
+
 @app.route("/all/get", methods=['GET'])
 def get_all():
     stmt = select(Shop)
@@ -179,7 +215,9 @@ def get_all():
     return json_resp(200, True, data)
 
 if __name__ == '__main__':
-    app = run_app()
+    Base.metadata.drop_all(engine)
+    sleep(1)
     Base.metadata.create_all(engine)
+    app = run_app()
 
     app.run(debug=True)
--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -8,17 +8,53 @@ const Game = () => {
   const [damage, setDamage] = useState(1);
   const [itemDamageLevel, setItemDamageLevel] = useState(0);
   const [itemDamagePrice, setItemDamagePrice] = useState(50);
-  const [itemAutoClickLevel, setItemAutoClickLevel] = useState(1);
+  const [itemAutoClickLevel, setItemAutoClickLevel] = useState(0);
   const [autoClickInterval, setAutoClickInterval] = useState(3300);
   const [itemAutoClickPrice, setItemAutoClickPrice] = useState(50);
 
   const BaseHP = () => level * 2;
+
+  // fetch on mount
+  useEffect(() => {
+    const fetchOnMount = async () => {
+      const res = await fetch("http://127.0.0.1:5000/all/get");
+      const data = await res.json();
+
+      setCoins(data.msg.coins.amount);
+      setLevel(data.msg.player.level);
+      setItemDamageLevel(data.msg.items[1].item_level);
+      setItemAutoClickLevel(data.msg.items[0].item_level);
+
+      setItemDamagePrice(data.msg.items[1].item_price);
+      setItemAutoClickPrice(data.msg.items[0].item_price);
+    };
+
+    fetchOnMount();
+  }, []);
+
+  const dataCoinLevelUp = async () => {
+    const resCoin = await fetch("http://127.0.0.1:5000/coins/gain", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ coins: 10 }),
+    });
+    const dataCoin = resCoin.json();
+    console.log(dataCoin);
+
+    const res = await fetch("http://127.0.0.1:5000/player/level_up");
+    const data = res.json();
+    console.log(data);
+  };
 
   useEffect(() => {
     if (hp <= 0) {
       setLevel((prev) => prev + 1);
       setHp(BaseHP);
       setCoins((prev) => prev + 10);
+      const data_in_func = dataCoinLevelUp();
+      console.log(data_in_func);
     }
   }, [hp]);
 
@@ -40,23 +76,72 @@ const Game = () => {
 
   // Shop
   // item Add Damage
-  const itemAddDamage = () => {
+  const itemAddDamage = async () => {
     if (coins >= itemDamagePrice) {
       setCoins((prev) => prev - itemDamagePrice);
       setDamage((d) => d + 1);
       setItemDamagePrice((prev) => Math.floor(prev * 1.5));
+
+      const res = await fetch("http://127.0.0.1:5000/shop/item_level", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: 2 }),
+      });
+      const data = res.json();
+      console.log(data);
+
+      const resprice = await fetch("http://127.0.0.1:5000/shop/item/price", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: 2,
+          price: itemDamagePrice,
+        }),
+      });
+      const dataprice = resprice.json();
+      console.log(dataprice);
     }
   };
 
   // item Auto Clicker
-  const buyAutoClicker = () => {
+  const buyAutoClicker = async () => {
     if (coins >= itemAutoClickPrice) {
       console.log("Buy Auto Clicker...");
       setCoins((prev) => prev - itemAutoClickPrice);
       setItemAutoClickLevel((prev) => prev + 1);
       setItemAutoClickPrice((prev) => Math.floor(prev * 1.5));
+      console.log(itemAutoClickPrice);
+
       if (autoClickInterval > 0.75) {
-        setAutoClickInterval(prev => prev - 300);
+        setAutoClickInterval((prev) => prev - 300);
+
+        const res = await fetch("http://127.0.0.1:5000/shop/item_level", {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id: 1 }),
+        });
+        const data = res.json();
+        console.log(data);
+
+        const resprice = await fetch("http://127.0.0.1:5000/shop/item/price", {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: 1,
+            price: itemAutoClickPrice,
+          }),
+        });
+        const dataprice = resprice.json();
+        console.log(itemAutoClickPrice);
+        console.log(dataprice);
       }
     }
   };
@@ -116,9 +201,7 @@ const Game = () => {
           <div>
             <h3>Item Auto Clicker</h3>
             <p>Auto Click per Seconds</p>
-            <span>
-              {autoClickInterval / 1000}s -0.3s
-            </span>
+            <span>{autoClickInterval / 1000}s -0.3s</span>
           </div>
 
           <div className="flex flex-col">
